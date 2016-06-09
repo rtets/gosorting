bd5a2bd7cff4d9a46a8a08f03a74ea5f210bbd5a
rtets <ricktetstall@gmail.com>
2016-06-09 02:04:08 +0100

    actually goodnight

--- src/github.com/rtets/gosorting/mergesort.go
+++ src/github.com/rtets/gosorting/mergesort.go
@@ -3,9 +3,11 @@ package main
 import (
     "fmt"
     "os"
+    "bufio"
+    "strconv"
 )
 
-func mergesort(input []int) {
+func mergesort(input []int) ([]int) {
     ilen := len(input)
     if (ilen<2) {
         return input
@@ -16,25 +18,64 @@ func mergesort(input []int) {
 
 func merge(left []int,right []int) (merged []int){
     llen,rlen := len(left), len(right)
-    mlen = llen+rlen
-    merged := make([]int, mlen)
-    l,r = 0,0
-    for (i := 0; i<mlen; i++) {
-        if (left[l] < right[r]) {
-
+    mlen := llen+rlen
+    merged = make([]int, mlen)
+    i,l,r := 0,0,0
+    for {
+        if (l==llen) {
+            copy(merged[i:mlen], right[r:rlen])
+            return
+        } else if (r==rlen) {
+            copy(merged[i:mlen], left[l:llen])
+            return
+        } else if (left[l] < right[r]) {
+            merged[i] = left[l]
+            l++
+        } else {
+            merged[i] = right[r]
+            r++
         }
+        i++
     }
+    return
 }
 
 func split(input []int, ilen int) (left []int, right []int){
     mid := ilen/2
-    left, right := make([]int, mid), make([]int, ilen-mid)
+    left, right = make([]int, mid), make([]int, ilen-mid)
     copy(left, input[0:mid])
     copy(right, input[mid:ilen])
     return
 }
 
 func main() {
-    fmt.Println("Hello world!")
+    // scanner := bufio.NewScanner(os.Stdin)
+    // unsorted := []int{}
+    // for scanner.Scan() {
+    //     i,err := strconv.Atoi(scanner.Text())
+    //     if (err==nil) {
+    //         unsorted = append(unsorted,i)
+    //     }
+    // }
+    scanner := bufio.NewScanner(os.Stdin)
+    index:=0
+    unsorted := make([]int, 1000000)
+    for scanner.Scan() {
+        v,err := strconv.Atoi(scanner.Text())
+        if (err==nil) {
+            unsorted[index] = v
+        }
+    }
+    unsorted = mergesort(unsorted)
+    for _,v := range unsorted {
+        fmt.Println(v)
+    }
+
+    // fmt.Println(os.Args[0])
+    // unsorted := []int{}
+    // fmt.Println(unsorted)
+    // unsorted = mergesort(unsorted)
+    // fmt.Println(unsorted)
+    
     
 }
